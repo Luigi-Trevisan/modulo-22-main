3730089e1f139f9a76fe329356a1cdcdf983cdb8
luigi <luigi.trevisan44@gmail.com>
2025-05-20 16:52:06 -0300

    trigger action

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
+        baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+  
   },
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'coverage',
+    overwrite: false,
+    html: true,
+    json: false
+  }
 });
